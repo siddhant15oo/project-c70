dbd3edf46988267af48c81652b4c51f185459b73
siddhant15oo <61053704+siddhant15oo@users.noreply.github.com>
2020-08-14 19:06:29 +0530

    Add files via upload

---
+++ WriteStory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+export default class WriteStory extends React.Component{
+  render(){
+      return{
+          
+      }
+  }
+}
+
+
